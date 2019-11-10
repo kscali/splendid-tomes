dc0d5149004218f028a6fae4fb9763fc654a76d8
koy <kosatun@aol.com>
2019-11-09 17:51:51 -0800

    removed logger from production build

--- src/store/store.js
+++ src/store/store.js
@@ -3,23 +3,21 @@ import rootReducer from "../reducers/rootReducer";
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from "redux-thunk";
-import logger from 'redux-logger';
 
 const persistConfig = {
   key: 'root',
-  storage,
-  // blacklist: ['books']
+  storage
 }
 
-// const middlewares = [thunk];
+const middlewares = [thunk];
 
-// if (process.env.NODE_ENV !== "production") {
-//   const { logger } = require("redux-logger");
-//   middlewares.push(logger);
-// }
+if (process.env.NODE_ENV !== "production") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const configureStore = createStore(persistedReducer, applyMiddleware(thunk, logger));
+export const configureStore = createStore(persistedReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(configureStore);
